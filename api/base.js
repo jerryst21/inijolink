e1133b34a1e878eac38d1c4a537cddeafe370279
jerryst21 <136833649+jerryst21@users.noreply.github.com>
2026-05-10 01:53:55 +0800

    fix links

--- api/base.js
+++ api/base.js
@@ -3,11 +3,11 @@ export default function handler(req, res) {
     {
       category: "TUGAS HARIAN",
       links: [
-        { title: "Pembukuan", url: "#", icon: "M", iconColor: "#ff4757" }, // Pakai FontAwesome
+        { title: "Pembukuan", url: "#", icon: "M" }, // iconColor: "#ff4757"
         { title: "BCA - VPN", url: "#", icon: "fa-solid fa-shield-halved", iconColor: "#1e90ff" }, // Pakai FontAwesome
         { title: "BNIDIRECT CASH", url: "#", icon: "<i class="fa-brands fa-cash-app"></i>", iconColor: "#FFA500" },
-        { title: "QLOLA By BRI", url: "#", icon: "BRI", iconColor: "#002147" },
-        { title: "Kopra By Mandiri", url: "#", icon: "K", iconColor: "#55A9DC" },
+        { title: "QLOLA By BRI", url: "#", icon: "BRI"}, // iconColor: "#002147" 
+        { title: "Kopra By Mandiri", url: "#", icon: "K"}, // iconColor: "#55A9DC" 
         { title: "Kas Kantor", url: "#", icon: "📗", status: "🟢",
           subLinks: [
             { label: "2025", url: "#" },
