b0564d2a50c94c377f353b7b53d10dc9ea3b3c5f
jerryst21 <136833649+jerryst21@users.noreply.github.com>
2026-05-10 01:51:58 +0800

    update link

--- api/base.js
+++ api/base.js
@@ -3,18 +3,18 @@ export default function handler(req, res) {
     {
       category: "TUGAS HARIAN",
       links: [
-        { title: "Pembukuan", url: "#", icon: "fa-solid fa-money-check", iconColor: "#ff4757" }, // Pakai FontAwesome
+        { title: "Pembukuan", url: "#", icon: "M", iconColor: "#ff4757" }, // Pakai FontAwesome
         { title: "BCA - VPN", url: "#", icon: "fa-solid fa-shield-halved", iconColor: "#1e90ff" }, // Pakai FontAwesome
-        { title: "BNIDIRECT CASH", url: "#", icon: "BNI" },
-        { title: "QLOLA By BRI", url: "#", icon: "BRI" },
-        { title: "Kopra By Mandiri", url: "#", icon: "MDR" },
+        { title: "BNIDIRECT CASH", url: "#", icon: "<i class="fa-brands fa-cash-app"></i>", iconColor: "#FFA500" },
+        { title: "QLOLA By BRI", url: "#", icon: "BRI", iconColor: "#002147" },
+        { title: "Kopra By Mandiri", url: "#", icon: "K", iconColor: "#55A9DC" },
         { title: "Kas Kantor", url: "#", icon: "📗", status: "🟢",
           subLinks: [
             { label: "2025", url: "#" },
             { label: "2026", url: "#" }
           ]
         },
-        { title: "Arsip Nota", url: "#", icon: "fa-solid fa-file-invoice" }, // Pakai FontAwesome
+        { title: "Arsip Nota", url: "#", icon: "📗" },
         { title: "Rekap Sales", url: "#", icon: "📗", status: "🔵",
           subLinks: [
             { label: "2025", url: "#" },
