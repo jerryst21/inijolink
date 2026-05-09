d1ff21e0a435ad74f570b03a7778de1925ec68a1
jerryst21 <136833649+jerryst21@users.noreply.github.com>
2026-05-10 00:58:39 +0800

    Ubah struktur json

--- api/base.js
+++ api/base.js
@@ -1,29 +1,50 @@
-// api/base.js
 export default function handler(req, res) {
   const data = [
     {
       category: "TUGAS HARIAN",
       links: [
-        { no: 1, title: "PEMBUKUAN", url: "#", icon: "M" },
-        { no: 2, title: "BCA - VPN", url: "#", icon: "BCA" },
-        { no: 3, title: "BNIDIRECT CASH", url: "#", icon: "BNI" },
-        { no: 4, title: "QLOLA BY BRI", url: "#", icon: "BRI" },
-        { no: 5, title: "KOPRA BY MANDIRI", url: "#", icon: "MDR" },
-        { no: 6, title: "KAS KANTOR - 2026", url: "#", icon: "📗", status: "🟢" },
-        { no: 7, title: "ARSIP NOTA", url: "#", icon: "📁" },
-        { no: 8, title: "REKAP SALES - 2026", url: "#", icon: "📗", status: "🔵" },
-        { no: 9, title: "STOK GUDANG & FREEZER 2026", url: "#", icon: "📗", status: "🟡" },
-        { no: 10, title: "DAFTAR AKTIVA", url: "#", icon: "📗", status: "🔵" },
+        { title: "PEMBUKUAN", url: "#", icon: "fi fi-sr-book", iconColor: "#ff4757" },
+        { title: "BCA - VPN", url: "#", icon: "fi fi-sr-vpn", iconColor: "#1e90ff" },
+        { title: "BNIDIRECT CASH", url: "#", icon: "BNI" },
+        { title: "QLOLA BY BRI", url: "#", icon: "BRI" },
+        { title: "KOPRA BY MANDIRI", url: "#", icon: "MDR" },
+        { 
+          title: "KAS KANTOR", 
+          url: "#", 
+          icon: "📗", 
+          status: "🟢",
+          subLinks: [
+            { label: "2025", url: "#" },
+            { label: "2026", url: "#" }
+          ]
+        },
+        { title: "ARSIP NOTA", url: "#", icon: "📁" },
+        { 
+          title: "REKAP SALES", 
+          url: "#", 
+          icon: "📗", 
+          status: "🔵",
+          subLinks: [
+            { label: "2025", url: "#" },
+            { label: "2026", url: "#" }
+          ]
+        },
       ]
     },
     {
       category: "TUGAS BULANAN",
       links: [
-        { no: 11, title: "BPJS KETENAGAKERJAAN", url: "#", icon: "⚙️", info: "10" },
-        { no: 12, title: "PERHITUNGAN GAJI 2026", url: "#", icon: "📗", status: "🔵" },
-        { no: 13, title: "REKAPAN INSENTIF", url: "#", icon: "📗", status: "🔵" },
-        { no: 14, title: "LAPORAN LABA RUGI", url: "#", icon: "📗", status: "🔵" },
-        { no: 15, title: "PROFIT SHARING", url: "#", icon: "📗", status: "🔵" },
+        { title: "BPJS KETENAGAKERJAAN", url: "#", icon: "⚙️", info: "Jatuh tempo tgl 10" },
+        { 
+          title: "PERHITUNGAN GAJI", 
+          url: "#", 
+          icon: "📗", 
+          status: "🔵",
+          subLinks: [
+            { label: "2025", url: "#" },
+            { label: "2026", url: "#" }
+          ]
+        },
       ]
     }
   ];
